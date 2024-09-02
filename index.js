a28655b2b37015b2fe05ade576dca2d50ffc274e
soubhari_ved <soubharived@gmail.com>
2024-09-02 21:43:57 +0530

    done

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require('express')
 const app = express()
 const mongoDB = require("./db")
 const dotenv = require('dotenv')
+var cors = require('cors')
+
 // const { query, matchedData, validationResult } = require('express-validator');
 dotenv.config()
 const port = process.env.BASE_URL
 mongoDB();
 
 
-app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-with, Content-Type, Accept"
-  );
-  next();
-})
+// app.use((req, res, next)=>{
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-with, Content-Type, Accept"
+//   );
+//   next();
+// })
+app.use(cors())
+
 
 app.use(express.json())
 app.use("/api", require("./Routes/CreateUser"))
